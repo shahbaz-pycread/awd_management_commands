7ca3017d4bf9b2f8c509d7e7da5af048eca12201
= <=>
2025-02-16 19:43:22 +0530

    Added Student model to admin

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name','roll_no','age']
+admin.site.register(Student, StudentAdmin)
