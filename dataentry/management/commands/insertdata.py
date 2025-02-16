f7eeddc86a4dd4c60bc56413d7cf08d7c5e7c5c8
= <=>
2025-02-16 21:20:06 +0530

    Added validation to check if the roll no already exists

--- dataentry/management/commands/insertdata.py
+++ dataentry/management/commands/insertdata.py
@@ -10,24 +10,24 @@ class Command(BaseCommand):
         # add static dataset => list of dictionary
         dataset = [
             {
-                'roll_no' : 1033,
-                'name' : 'Alam',
-                'age' : 31
+                'roll_no' : 1001,
+                'name' : 'Abhishek',
+                'age' : 34
             },
             {
-                'roll_no' : 1018,
-                'name' : 'Chirag',
+                'roll_no' : 1032,
+                'name' : 'Mayuri',
                 'age' : 33
             },
             {
-                'roll_no' : 1017,
-                'name' : 'Chinmay',
-                'age' : 33
+                'roll_no' : 1002,
+                'name' : 'Ajay',
+                'age' : 34
             },
             {
-                'roll_no' : 1053,
-                'name' : 'Shashank',
-                'age' : 30
+                'roll_no' : 1069,
+                'name' : 'Sreesh',
+                'age' : 34
             },
             {
                 'roll_no' : 1049,
@@ -36,7 +36,15 @@ class Command(BaseCommand):
             }
         ]
         for data in dataset:
-            Student.objects.create(roll_no=data['roll_no'], name=data['name'], age=data['age'])
+            # data validation
+            # by roll no
+            roll_no = data['roll_no']
+            #checking if roll_no exists or not
+            existing_record = Student.objects.filter(roll_no=roll_no).exists()
+            if not existing_record:
+                Student.objects.create(roll_no=data['roll_no'], name=data['name'], age=data['age'])
+            else:
+                self.stdout.write(self.style.WARNING(f'Student with roll no {roll_no} already exists'))
 
         self.stdout.write(self.style.SUCCESS('Data inserted successfully.'))
         
