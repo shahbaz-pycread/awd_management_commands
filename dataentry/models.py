794212d44a30cae8846ab4e0569227a28b3318f7
= <=>
2025-02-17 19:08:22 +0530

    Added new models 'Customer'

--- dataentry/models.py
+++ dataentry/models.py
@@ -7,3 +7,10 @@ class Student(models.Model):
     
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.customer_name
