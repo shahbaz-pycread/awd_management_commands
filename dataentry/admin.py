76a579eac797eef418613a81251d9d9831db4b4d
= <=>
2025-02-17 19:30:24 +0530

    Added Customer Models

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Customer
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name','roll_no','age']
 admin.site.register(Student, StudentAdmin)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['customer_name', 'country']
+    list_filter = ['country']
+    search_fields = ['country']
+    
+admin.site.register(Customer, CustomerAdmin)
